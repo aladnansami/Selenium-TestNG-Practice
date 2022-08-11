1a8ef0828e932a3f95e147b73002a4285bb2d05a
aladnansami <aladnansami21@gmail.com>
2022-08-11 15:53:49 +0600

    first commit

---
+++ src/test/java/Pages/OrderPage.java
@@ -0,0 +1,52 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class OrderPage {
+    @FindBy(id="search_query_top")
+    WebElement txtSearch;
+    @FindBy(name="submit_search")
+    WebElement btnSearch;
+    @FindBy(className = "heading-counter")
+    WebElement lblResult;
+    @FindBy(tagName = "img")
+    List<WebElement> imgProduct;
+    @FindBy(xpath = "//span[contains(text(),'Add to cart')]")
+    WebElement btnAddCart;
+    @FindBy(css = "[title='Proceed to checkout']")
+    WebElement btnCheckout;
+    @FindBy(className = "page-heading")
+    WebElement lblSummaryPage;
+    WebDriver driver;
+
+    public OrderPage(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    public String searchProduct(String product){
+        txtSearch.sendKeys(product);
+        btnSearch.click();
+        return lblResult.getText();
+
+    }
+    public Boolean chooseProduct(){
+        imgProduct.get(14).click();
+        return btnAddCart.isDisplayed();
+    }
+    public Boolean addToCart(){
+        btnAddCart.click();
+        return btnCheckout.isDisplayed();
+    }
+    public String checkProductSumary(){
+        btnCheckout.click();
+        return lblSummaryPage.getText();
+
+    }
+}
---
+++ src/test/java/TestRunner/OrderTestRunner.java
@@ -0,0 +1,42 @@
+package TestRunner;
+
+import Base.Setup;
+import Pages.OrderPage;
+import io.qameta.allure.Allure;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.xml.ws.soap.Addressing;
+
+public class OrderTestRunner extends Setup {
+    OrderPage orderPage;
+    @Test(priority = 1,description = "User can search product")
+    public void searchProduct(){
+        driver.get("http://automationpractice.com/");
+        orderPage=new OrderPage(driver);
+        String res= orderPage.searchProduct("dress");
+        Assert.assertTrue(res.contains("results have been found"));
+        Allure.description("User can search product successfully");
+    }
+    @Test(priority = 2, description = "User can choose product")
+    public void chooseProduct(){
+        orderPage=new OrderPage(driver);
+        boolean found= orderPage.chooseProduct();
+        Assert.assertTrue(found);
+        Allure.description("User can choose product from the product list");
+    }
+    @Test(priority = 3, description = "User can add cart successfully")
+    public void addToCart(){
+        orderPage=new OrderPage(driver);
+        boolean found= orderPage.addToCart();
+        Assert.assertTrue(found);
+        Allure.description("User added the product into card");
+    }
+    @Test(priority = 4, description = "User can see product summary")
+    public void checkProductSumary(){
+        orderPage=new OrderPage(driver);
+        String summaryText= orderPage.checkProductSumary();
+        Assert.assertTrue(summaryText.contains("SHOPPING-CART SUMMARY"));
+        Allure.description("User can view product summary before checkout");
+    }
+}
